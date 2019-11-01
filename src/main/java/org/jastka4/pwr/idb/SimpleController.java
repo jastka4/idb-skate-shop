76b27c42d909ef582eb97c9d253ef62cdaffcb4a
Justyna Skalska <jastka4@gmail.com>
2019-11-01 18:14:13 +0100

    Initial commit

---
+++ src/main/java/org/jastka4/pwr/idb/SimpleController.java
@@ -0,0 +1,16 @@
+package org.jastka4.pwr.idb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", "test name");
+        return "home";
+    }
+}
---
+++ src/test/java/org/jastka4/pwr/idb/IdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jastka4.pwr.idb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
